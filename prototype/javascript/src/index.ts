f1f7c758f6f5edebcbaefe412452f0ba9c4dd918
Erik Rospo <rerik.work@gmail.com>
2023-01-26 14:25:16 -0800

    More changes on the prototype

--- prototype/javascript/src/index.ts
+++ prototype/javascript/src/index.ts
@@ -1,35 +1,36 @@
 // import * as _ from 'lodash';
 import * as PIXI from 'pixi.js';
 
-// function component() {
-//     const element = document.createElement('div');
-//     // Lodash, now imported by this script
-//     element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-
-//     return element;
-//   }
-
-//   document.body.appendChild(component());
+const canvas = document.createElement('canvas');
+const ctx=canvas.getContext("2d");
+document.body.appendChild(canvas);
 type bool2d = boolean[][];
 let grid: bool2d = [];
 let width = 720;
-let height = 480
-let grid_width = 10;
-let grid_height = 10;
-for (let x = 0; x < width / grid_width; x++) {
-  let temp_array: boolean[]
-  for (let y = 0; y < height / grid_height; y++) {
+let height =480
+canvas.width=width;
+canvas.height=height;
+let grid_size = 10;
+
+for (let x = 0; x < width / grid_size; x++) {
+  let temp_array: boolean[]=[];
+  for (let y = 0; y < height / grid_size; y++) {
     temp_array.push(false)
   }
   grid.push(temp_array)
 }
+function drawSquare(x: number,y: number,s: number,state:boolean=false): void{
+  ctx.fillStyle=state?"black":"white";
+  ctx.fillRect(x,y,s,s);
+  
+}
 function updateGol(grid: bool2d): bool2d {
-  let newgrid: bool2d
+  let newgrid: bool2d=[];
 
 
-  for (let x = 0; x < width / grid_width; x++) {
-    let temp_array: boolean[]
-    for (let y = 0; y < height / grid_height; y++) {
+  for (let x = 0; x < width / grid_size; x++) {
+    let temp_array: boolean[]=[];
+    for (let y = 0; y < height / grid_size; y++) {
       let total: number = 0;
       total += grid[x - 1][y + 1] ? 1 : 0
       total += grid[x - 1][y - 1] ? 1 : 0
@@ -48,7 +49,20 @@ function updateGol(grid: bool2d): bool2d {
         temp_array.push(false)
       }
     }
-    grid.push(temp_array)
+    newgrid.push(temp_array)
   }
   return newgrid;
 }
+function render(rendergrid:bool2d=grid){
+  for (let x = 0; x < width; x+=grid_size) {
+    for (let y=0;y<height;y+=grid_size){
+      drawSquare(x,y,grid_size,rendergrid[x/grid_size][y/grid_size])
+
+    }    
+  }
+}
+function step(){
+  grid=updateGol(grid);
+  render(grid)
+}
+setTimeout(()=>{step()},500)
